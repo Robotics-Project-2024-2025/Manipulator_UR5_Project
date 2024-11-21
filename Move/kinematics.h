49efac41bfbaa7ccd6e9d1922920c39e1975a84b
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-11-21 14:35:32 +0100

    Update kinematics.h

--- Move/kinematics.h
+++ Move/kinematics.h
@@ -19,6 +19,8 @@ using namespace std;
 using namespace Eigen;
 using MatrixD6=Matrix<double, -1, 6>;
 using Matrix61=Matrix<double, 6, 1>;
+using Matrix86=Matrix<double, 8, 6>;
+using Matrix44=Matrix<double, 4, 4>;
 MatrixD6 p2pMotionPlan(Vector3d xEs, Vector3d xEf, Vector3d phiEs, Vector3d phiEf, double minT, double maT, double dt);
 //Rotations
 Matrix3d xRot(double theta);
@@ -26,6 +28,8 @@ Matrix3d yRot(double theta);
 Matrix3d zRot(double theta);
 //Direct Kinematics
 pair<Vector3d, Matrix3d> Ur5Direct(Matrix61 Th);
+
+Matrix44 HomogeneousTransform(double theta, double alpha, double d, double a); 
 
 
 #endif /* kinematics_h */
