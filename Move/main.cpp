d110eaffd83d27c34b602d828c5ca55481ba04fc
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-11-21 13:03:38 +0100

    Create main.cpp

---
+++ Move/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Kinematics
+//
+//  Created by Matteo Gottardelli on 21/11/24.
+//
+
+#include <iostream>
+#include "kinematics.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <Eigen/Dense>
+#define DELTAT 0.01
+using namespace std;
+using namespace Eigen;
+int main(int argc, const char * argv[]) {
+    Vector3d xE0;
+    xE0 << 0.1, 0.3, 0.1;
+    Vector3d xE1;
+    xE1 << 0.5, 0.1, 0.5;
+    Vector3d phiE0;
+    phiE0 << 0, 0, 0;
+    Vector3d phiE1;
+    phiE1 << M_PI/6, M_PI/3, M_PI/4;
+    MatrixD6 th=p2pMotionPlan(xE0, xE1, phiE0, phiE1, 0, 1, DELTAT);
+    cout << th << endl;
+    return 0;
+}
