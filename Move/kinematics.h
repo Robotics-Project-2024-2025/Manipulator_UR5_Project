4d264e6c31b3a3b2c523c1456d448e9bb8a7ae29
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-11-21 13:44:19 +0100

    Update kinematics.h

--- Move/kinematics.h
+++ Move/kinematics.h
@@ -18,11 +18,14 @@
 using namespace std;
 using namespace Eigen;
 using MatrixD6=Matrix<double, -1, 6>;
+using Matrix61=Matrix<double, 6, 1>;
 MatrixD6 p2pMotionPlan(Vector3d xEs, Vector3d xEf, Vector3d phiEs, Vector3d phiEf, double minT, double maT, double dt);
-
+//Rotations
 Matrix3d xRot(double theta);
 Matrix3d yRot(double theta);
 Matrix3d zRot(double theta);
+//Direct Kinematics
+pair<Vector3d, Matrix3d> Ur5Direct(Matrix61 Th);
 
 
 #endif /* kinematics_h */
