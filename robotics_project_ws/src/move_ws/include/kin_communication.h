0db9fc47b6c4807a503d4d124116bfac08027d43
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-03 15:09:17 +0100

    Update kin_communication.h

--- robotics_project_ws/src/move_ws/include/kin_communication.h
+++ robotics_project_ws/src/move_ws/include/kin_communication.h
@@ -8,12 +8,20 @@
 #ifndef kin_communication_h
 #define kin_communication_h
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/init_options.hpp>
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
 #include <trajectory_msgs/msg/joint_trajectory_point.hpp>
 #include <std_msgs/msg/header.hpp>
 #include <iostream>
 #include <Eigen/Dense>
 #include <chrono>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <rcutils/allocator.h>
+#include <tracetools/tracetools.h>
+#include <memory>
+
 /*
  # The header is used to specify the coordinate frame and the reference time for
  # the trajectory durations
@@ -33,6 +41,7 @@
 #define WRIST_1 "wrist_1_joint"
 #define WRIST_2 "wrist_2_joint"
 #define WRIST_3 "wrist_3_joint"
+#define NUM_JOINTS 6
 using namespace std;
 using namespace Eigen;
 using MatrixD6=Matrix<double, -1, 6>;
@@ -42,51 +51,70 @@ using namespace std::chrono_literals;
 class TrajectoryPublisher : public rclcpp::Node
 {
 public:
-    TrajectoryPublisher() : Node("trajectory_publisher")
+    TrajectoryPublisher(MatrixD6 Th) : Node("trajectory_publisher")
     {
-         increment_ = 0.0;  // Initialize the increment
         // Create a publisher for the scaled joint trajectory controller
         trajectory_publisher_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>(
             "/scaled_joint_trajectory_controller/joint_trajectory", 10);
 
         // Create a timer that will periodically publish a trajectory
         timer_ = this->create_wall_timer(
-            1s, std::bind(&TrajectoryPublisher::publish_trajectory, this));
+                                         1s, [this, Th]() {this->publish_iter(Th);});
     }
 
 private:
-    void publish_trajectory()
+    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr trajectory_publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    void publish_iter(MatrixD6 Th)
     {
-        // Create the JointTrajectory message
         trajectory_msgs::msg::JointTrajectory traj_msg;
-        traj_msg.header.stamp = this->now();  // Set the current timestamp
-        traj_msg.header.frame_id = "base_link";  // Set the appropriate reference frame
-
-        // Define the joint names for the trajectory
-        traj_msg.joint_names = {"shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"};
-
-        // Create a joint trajectory point
-        trajectory_msgs::msg::JointTrajectoryPoint point;
-        point.positions = {0.17 + increment_, -1.41, 1.84, -0.43, 0.17, -0.38};  // Example positions for each joint
-        point.velocities = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};  // Zero velocities
-        point.accelerations = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};  // Zero accelerations
-
-        increment_ += 0.1;  // Increment the position for the next trajectory
-
-        // Set the time from the start for each point (e.g., 2 seconds)
-        point.time_from_start = rclcpp::Duration(2.0s);
-
-        // Add the point to the trajectory message
-        traj_msg.points.push_back(point);
-
+        init_Trajectory(&traj_msg);
+        naming_Points(&traj_msg);
+        for (int j=0; j<6; j++) {
+            trajectory_msgs::msg::JointTrajectoryPoint point;
+            point.time_from_start=rclcpp::Duration(2.0s);
+            for (int i=0; i<Th.rows(); i++) {
+                point.positions.push_back(Th(i, j));
+                point.velocities.push_back(0.0);
+                point.accelerations.push_back(0.0);
+                point.effort.push_back(0.0);
+                add_point(&traj_msg, point);
+                cout << i*100+j << endl;
+            }
+        }
+        publish_trajectory(traj_msg);
+    }
+    void init_Trajectory(trajectory_msgs::msg::JointTrajectory* traj_msg) {
+        traj_msg->header.stamp=this->now();
+        traj_msg->header.frame_id = "base_link";
+        
+    }
+    string chooseName(int index) {
+        string tmp;
+        switch(index) {
+            case 0: tmp=SHOULDER_PAN; break;
+            case 1: tmp=SHOULDER_LIFT; break;
+            case 2: tmp=ELBOW; break;
+            case 3: tmp=WRIST_1; break;
+            case 4: tmp=WRIST_2; break;
+            case 5: tmp=WRIST_3; break;
+            default: exit(1);
+        }
+        return tmp;
+    }
+    void naming_Points(trajectory_msgs::msg::JointTrajectory* traj_msg) {
+        for (int i=0; i<NUM_JOINTS; i++) {
+            traj_msg->joint_names.push_back(chooseName(i));
+        }
+    }
+    void add_point(trajectory_msgs::msg::JointTrajectory* traj_msg, trajectory_msgs::msg::JointTrajectoryPoint p) {
+        traj_msg->points.push_back(p);
+    }
+    void publish_trajectory(trajectory_msgs::msg::JointTrajectory traj_msg) {
         // Publish the trajectory
         trajectory_publisher_->publish(traj_msg);
         RCLCPP_INFO(this->get_logger(), "Published trajectory with %zu points", traj_msg.points.size());
     }
-
-    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr trajectory_publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    double increment_;
 };
 
 #endif /* kin_communication_h */
