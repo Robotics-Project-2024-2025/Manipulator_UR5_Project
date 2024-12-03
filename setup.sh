88a32f0255845f3d751723e6b8b8b94814c1aa53
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-03 14:19:25 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd /home/ubuntu/ros2_ws/
+
+rosdep install -i --from-path src --rosdistro rolling -y
+
+colcon build --packages-select ur5_simulation
+
+source install/setup.bash
+
