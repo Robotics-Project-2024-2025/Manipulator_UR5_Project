b85d8eb19d9d9f3fc0b0f3579ad052fdd4a48e2a
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-04 19:18:24 +0100

    Update setupBena.sh

--- setupBena.sh
+++ setupBena.sh
@@ -3,4 +3,8 @@ cd /home/ubuntu/ros2_ws/
 
 rosdep install -i --from-path src --rosdistro rolling -y
 
-colcon build --packages-select ros2_ur5_interface
+rm -rf ./build ./install ./log
+
+colcon build
+
+source install/setup.bash
