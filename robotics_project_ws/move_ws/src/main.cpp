22aa7bd679c587705bbe4615ab4dc0e81c744ea7
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-05 12:33:46 +0100

    Update main.cpp

--- robotics_project_ws/move_ws/src/main.cpp
+++ robotics_project_ws/move_ws/src/main.cpp
@@ -13,11 +13,10 @@ using namespace Eigen;
 int main(int argc, const char * argv[]) {
     /*Vector3d xE0;
     xE0 << 0.1, 0.3, 0.1;*/
-   setupCommunication(argc, argv);
+    setupCommunication(argc, argv);
     while(rclcpp::ok()) {
         cout << "Waiting for Joint States" << endl;
         Vector3d xE1;
-        xE1 << 0.0, 0.0, 0.0;
         Matrix16 qES;
         qES=receive_joint_state();
         /*Vector3d phiE0;
@@ -28,9 +27,8 @@ int main(int argc, const char * argv[]) {
         cout << endl << "Insert values for xE1:" << endl;
         for(int i = 0; i<3; i++){
             double tmp;
-            cout << "Insert Value " << i << ": ";
             cin >> tmp;
-            if(tmp >= -1.0 && tmp <= 1.0){
+            if(tmp >= -3 && tmp <= 3){
                 xE1(i) = tmp;
             }
             else{
@@ -40,6 +38,7 @@ int main(int argc, const char * argv[]) {
         if(checkPosition(xE1, qES)) {
             if(p2pMotionPlan(qES, xE1, phiE1, &th)) {
                 cout << "Moving to HOME" << endl;
+                setupCommunication(argc, argv);
                 send_trajectory(th);
             }
             else {
@@ -53,6 +52,7 @@ int main(int argc, const char * argv[]) {
         for(int i=0; i<3; i++){
             cout << endl << xE1(i) << " ";
         }
+        setupCommunication(argc, argv);
     }
     return 0;
 }
