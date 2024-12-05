1bd2d9de1e9c499d99e0755a829e4d417063b955
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-05 11:09:00 +0100

    Update main.cpp

--- robotics_project_ws/move_ws/src/main.cpp
+++ robotics_project_ws/move_ws/src/main.cpp
@@ -13,13 +13,12 @@ using namespace Eigen;
 int main(int argc, const char * argv[]) {
     /*Vector3d xE0;
     xE0 << 0.1, 0.3, 0.1;*/
+    while(rclcpp::ok()) {
     setupCommunication(argc, argv);
     cout << "Waiting for Joint States" << endl;
-    
     Vector3d xE1;
-    xE1 << 0.1, 0.3, -0.4;
     Matrix16 qES;
-    qES << 0, -M_PI/2+0.01, 0, 0, 0, 0;
+    qES=receive_joint_state();
     /*Vector3d phiE0;
     phiE0 << 0, 0, 0;*/
     Vector3d phiE1;
@@ -48,18 +47,9 @@ int main(int argc, const char * argv[]) {
     else {
         cout << "Impossible to Reach the final Position " << xE1 << endl;
     }
-
-    cout << "Control vector xE1 integrity:"
+    cout << "Control vector xE1 integrity:";
     for(int i=0; i<3; i++){
         cout << endl << xE1(i) << " ";
     }
-    
-    /*const rclcpp::InitOptions iO(rcl_get_default_allocator());
-    if(!rclcpp::ok()) {
-        rclcpp::init(static_cast<int>(argc), argv, rclcpp::InitOptions(),
-                     rclcpp::SignalHandlerOptions::None);
-    }
-    rclcpp::spin(std::make_shared<TrajectoryPublisher>(th));
-    rclcpp::shutdown();*/
     return 0;
 }
