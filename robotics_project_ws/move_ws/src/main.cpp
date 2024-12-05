a0001d01065723e097958041152b4c1555ac9216
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-12-05 15:34:46 +0100

    Update main.cpp

--- robotics_project_ws/move_ws/src/main.cpp
+++ robotics_project_ws/move_ws/src/main.cpp
@@ -7,6 +7,7 @@
 
 #include "kinematics.h"
 #include "kin_communication.h"
+#include "path.h"
 
 using namespace std;
 using namespace Eigen;
@@ -18,44 +19,13 @@ int main(int argc, const char * argv[]) {
         cout << "Waiting for Joint States" << endl;
         Vector3d xE1;
         xE1 << 0.0, 0.0, 0.0;
-        Matrix16 qES;
-        qES=receive_joint_state();
-        cout << qES << endl;
         /*Vector3d phiE0;
          phiE0 << 0, 0, 0;*/
         Vector3d phiE1;
         phiE1 << 0, 0, 0;
-        MatrixD6 th;
-        for(int i = 0; i<3; i++){
-            double tmp;
-            cout << "Insert value " << i+1 << "for xE1: ";
-            cin >> tmp;
-            if(tmp >= -1 && tmp <= 1){
-                xE1(i) = tmp;
-            }
-            else{
-                cout << endl << "Invalid value" << endl;
-            }
+        if(!path(xE1, phiE1)){
+            cout << "Errore!" << endl;
         }
-        cout << "Checking Position" << endl;
-        if(checkPosition(xE1, qES)) {
-            if(p2pMotionPlan(qES, xE1, phiE1, &th)) {
-                cout << "Moving to HOME" << endl;
-                setupCommunication(argc, argv);
-                send_trajectory(th);
-            }
-            else {
-                cout << "Error in computation of trajectory to point " << xE1 << endl;
-            }
-        }
-        else {
-            cout << "Impossible to Reach the final Position " << xE1 << endl;
-        }
-        cout << "Control vector xE1 integrity:";
-        for(int i=0; i<3; i++){
-            cout << endl << xE1(i) << " ";
-        }
-        setupCommunication(argc, argv);
     }
     return 0;
 }
