b0892ba5ed5ce8b746a52af1ab9dcdceb99b5c15
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-05 11:31:06 +0100

    Update kin_communication.cpp

--- robotics_project_ws/move_ws/src/kin_communication.cpp
+++ robotics_project_ws/move_ws/src/kin_communication.cpp
@@ -112,7 +112,7 @@ void TrajectoryActionClient::publish_trajectory(trajectory_msgs::msg::JointTraje
 }
 
 JointReceiver::JointReceiver() : Node("arm_receiver") {
-    joint_receiver_ = this->create_subscription<sensors_msgs::msg::JointState>("/joint_states", 10, [this](shared_ptr<sensor_msgs::msg::JointState> msg) {
+    joint_receiver_ = this->create_subscription<sensor_msgs::m sg::JointState>("/joint_states", 10, [this](shared_ptr<sensor_msgs::msg::JointState> msg) {
         joint_state_=msg;
         RCLCPP_INFO(this->get_logger(), "Received Joint State message");
     });
@@ -122,7 +122,6 @@ JointReceiver::JointReceiver() : Node("arm_receiver") {
     if(joint_state_) {
         RCLCPP_INFO(this->get_logger(), "Successfully Received Joint State");
     }
-    publish_iter(Th);
 }
 
 shared_ptr<sensor_msgs::msg::JointState> get_joint_state() const {
