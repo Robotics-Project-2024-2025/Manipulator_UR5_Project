6c5dda480423f5470bdff3dc710f4a40166ec35a
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-12-05 09:22:05 +0100

    Update setupVRizBena.sh

--- setupVRizBena.sh
+++ setupVRizBena.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu/ros2_ws/
 
 rosdep install -i --from-path src --rosdistro rolling -y
 
-colcon build --packages-select ros2_ur5_simulation
+colcon build --packages-select ros2_ur5_interface
 
 source install/setup.bash
 
