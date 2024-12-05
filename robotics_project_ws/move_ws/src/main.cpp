b6d68e7e0114f26260f2ced4d7504be18c0b10b3
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-05 11:21:30 +0100

    Update main.cpp

--- robotics_project_ws/move_ws/src/main.cpp
+++ robotics_project_ws/move_ws/src/main.cpp
@@ -14,42 +14,43 @@ int main(int argc, const char * argv[]) {
     /*Vector3d xE0;
     xE0 << 0.1, 0.3, 0.1;*/
     while(rclcpp::ok()) {
-    setupCommunication(argc, argv);
-    cout << "Waiting for Joint States" << endl;
-    Vector3d xE1;
-    Matrix16 qES;
-    qES=receive_joint_state();
-    /*Vector3d phiE0;
-    phiE0 << 0, 0, 0;*/
-    Vector3d phiE1;
-    phiE1 << 0, 0, 0;
-    MatrixD6 th;
-    cout << endl << "Insert values for xE1:" << endl;
-    for(int i = 0; i<3; i++){
-        double tmp;
-        cin >> tmp;
-        if(tmp >= -3 && tmp <= 3){
-            xE1(i) = tmp;
+        setupCommunication(argc, argv);
+        cout << "Waiting for Joint States" << endl;
+        Vector3d xE1;
+        Matrix16 qES;
+        qES=receive_joint_state();
+        /*Vector3d phiE0;
+         phiE0 << 0, 0, 0;*/
+        Vector3d phiE1;
+        phiE1 << 0, 0, 0;
+        MatrixD6 th;
+        cout << endl << "Insert values for xE1:" << endl;
+        for(int i = 0; i<3; i++){
+            double tmp;
+            cin >> tmp;
+            if(tmp >= -3 && tmp <= 3){
+                xE1(i) = tmp;
+            }
+            else{
+                cout << endl << "Invalid value" << endl;
+            }
         }
-        else{
-            cout << endl << "Invalid value" << endl;
-        }
-    }
-    if(checkPosition(xE1, qES)) {
-        if(p2pMotionPlan(qES, xE1, phiE1, &th)) {
-            cout << "Moving to HOME" << endl;
-            send_trajectory(th);
+        if(checkPosition(xE1, qES)) {
+            if(p2pMotionPlan(qES, xE1, phiE1, &th)) {
+                cout << "Moving to HOME" << endl;
+                send_trajectory(th);
+            }
+            else {
+                cout << "Error in computation of trajectory to point " << xE1 << endl;
+            }
         }
         else {
-            cout << "Error in computation of trajectory to point " << xE1 << endl;
+            cout << "Impossible to Reach the final Position " << xE1 << endl;
         }
-    }
-    else {
-        cout << "Impossible to Reach the final Position " << xE1 << endl;
-    }
-    cout << "Control vector xE1 integrity:";
-    for(int i=0; i<3; i++){
-        cout << endl << xE1(i) << " ";
+        cout << "Control vector xE1 integrity:";
+        for(int i=0; i<3; i++){
+            cout << endl << xE1(i) << " ";
+        }
     }
     return 0;
 }
