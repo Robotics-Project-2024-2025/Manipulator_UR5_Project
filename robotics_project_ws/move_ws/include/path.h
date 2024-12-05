ea78e8eb407bedec5e837a0d6e691afc42563d77
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-12-05 15:36:10 +0100

    Add files via upload

---
+++ robotics_project_ws/move_ws/include/path.h
@@ -0,0 +1,22 @@
+//
+//  path.h
+//  Robotics
+//
+//  Created by Niccolò Cristoforetti on 05/12/24.
+//
+
+#ifndef path_h
+#define path_h
+#include <iostream>
+#include "kinematics.h"
+#include "kin_communication.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <Eigen/Dense>
+
+bool path(Vector3d xE1, Vector3d phiE1);
+
+#endif /* path_h */
+
