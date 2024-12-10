567a8aa0be0b79d39633cdd93abd3bd6aef6a5b0
Matteo Gottardelli <matteogottardelli@gmail.com>
2024-12-10 13:48:55 +0100

    Update

--- init.sh
+++ init.sh
@@ -16,11 +16,11 @@ fi
 
 run_in_terminator() {
     local script="$1"
-    if [ "$script" == "$SCRIPT1" ]; then
+    # if [ "$script" == "$SCRIPT1" ]; then
         terminator -T "$script" -x bash -c "./$script; exec bash" &
-    else
-         terminator --split-horizontal --command=bash -c "./$script; exec bash" &
-    fi
+    #else
+    #     terminator --new-tab -e "bash -c './$script; exec bash'" &
+   # fi
     # Wait for the user to close the Terminator window before continuing
     read -p "Press Enter when '$script' is done to continue..." </dev/tty
 }
