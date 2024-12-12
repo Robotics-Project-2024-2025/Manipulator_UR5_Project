9f2a263b523adc4a38f4138c962ccefb75c59a2d
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-12-12 17:09:35 +0100

    Update calc_medium_value.cpp

--- robotics_project_ws/camera_ws/src/calc_medium_value.cpp
+++ robotics_project_ws/camera_ws/src/calc_medium_value.cpp
@@ -8,8 +8,8 @@
 #include "calc_medium_value.h"
 #include <filesystem>
 #include <opencv2/opencv.hpp>
-#include "image_processing_interfaces/srv/DepthGet.srv"
-#include "image_processing_interfaces/msg/Point2D.msg"
+#include "srv/DepthGet.srv"
+#include "msg/Point2D.msg"
 
 ImageCamera::ImageCamera() : Node("image_acquiring") {
     image_receiver_ = this->create_subscription<senseimage>(
