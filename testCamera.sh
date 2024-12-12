46b24ea1765e98d6a522a2c244c6e4250b12dd12
Gotta003 <94313973+Gotta003@users.noreply.github.com>
2024-12-12 14:07:59 +0100

    Update testCamera.sh

--- testCamera.sh
+++ testCamera.sh
@@ -10,3 +10,6 @@ source install/setup.bash
 ros2 run camera_ws_pkg camera
 ros2 run camera_ws_pkg points_camera
 ros2 run camera_ws_pkg medium_val
+
+cd /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/vision_ws/yolov5
+python detect.py --source /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/camera_ws/generated/ --weights /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/vision_ws/blockTrain.pt --conf 0.25
