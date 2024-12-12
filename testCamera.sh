7d2e622588fd60177684d0885c7895d5931d73b5
Matteo Gottardelli <matteogottardelli@gmail.com>
2024-12-12 14:42:23 +0100

    Modify testCamera

--- testCamera.sh
+++ testCamera.sh
@@ -12,4 +12,12 @@ ros2 run camera_ws_pkg points_camera
 ros2 run camera_ws_pkg medium_val
 
 cd /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/vision_ws/yolov5
-python detect.py --source /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/camera_ws/generated/ --weights /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/vision_ws/blockTrain.pt --conf 0.25
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+sudo apt update
+sudo apt install python3-xyz
+python detect.py \
+  --source /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/camera_ws/generated/ \
+  --weights /home/ubuntu/ros2_ws/src/Project/robotics_project_ws/vision_ws/blockTrain.pt \
+  --conf 0.5
