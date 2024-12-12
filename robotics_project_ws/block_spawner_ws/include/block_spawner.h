20c777f1ccee82756f497b4b9a711f170e39185a
Matteo Gottardelli <matteogottardelli@gmail.com>
2024-12-12 12:02:22 +0100

    Blocks Section Update

---
+++ robotics_project_ws/block_spawner_ws/include/block_spawner.h
@@ -0,0 +1,90 @@
+#ifndef __BLOCK_SPAWNER_H__
+#define __BLOCK_SPAWNER_H__
+
+#include <rclcpp.hpp>
+#include <string>
+#include <Eigen/Dense>
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include "gazebo/gazebo.hh"
+#include <gazebo_msgs/SpawnModel.h>
+#include <gazebo_msgs/SetModelState.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <gazebo_msgs/GetWorldProperties.h>
+#include <gazebo_msgs/DeleteModel.h>
+#include <tf/transform_datatypes.h>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+#define NUM_BLOCKS 5
+#define MINIMUM_DISTANCE_BLOCK 0.15
+#define BORDER_DISTANCE 0.05
+#define DISTANCE_BOTTOM 0.4
+using namespace std;
+using namespace gazebo {
+struct BlockSize {
+    double x;
+    double y;
+    double z;
+};
+struct BlockPosition {
+    string name;
+    Vector3d wPosition;
+}
+class BlockSpawn : public rclcpp::Node : public WorldPlugin {
+public:
+    BlockSpawn(const string& path);
+    void Load(physics::WorldPtr _parent, sdf::ElementPtr) {
+        initVariables(physics::WorldPtr _parent);
+        generateBlocks();
+        block_spawner_=this->create_subscription<msgs::Factory>("/block_spawner_node/spawn", 100, [this](shared_ptr<msgs::Factory> msg){
+            factory_= const_pointer_cast<const msgs::Factory>(msg);
+            RCLCPP_INFO(this->get_logger(), "Set Block Spawning Message");
+        });
+        while (rclcpp::ok() && !block_spawner_) {
+            rclcpp::spin_some(this->get_node_base_interface());
+        }
+        if(block_spawner_) {
+            RCLCPP_INFO(this->get_logger(), "Successfully Spawned Blocks");
+            cout << "Successfully Spawned Blocks" << endl;
+        }
+    }
+    
+private:
+    vector<string> validBlocks;
+    map<string, gazebo::BlockSize> brickDefinition;
+    physics::WorldPtr worldPtr;
+    shared_ptr<rclcpp::Subscription<msgs::Factory>> block_spawner_;
+    shared_ptr<const msgs::Factory> factory_;
+    vector<gazebo::BlockPosition> addedBuffer;
+    vector<Vector3d> populateVector() {
+        //To implement reading in C++
+    }
+    void initVariables(physics::WorldPtr _parent) {
+        this->validBlocks.clear();
+        vector<string> names={"X1-Y1-Z2", "X1-Y2-Z1", "X1-Y2-Z2-CHAMFER", "X1-Y2-Z2-TWINFILLET", "X1-Y2-Z2", "X1-Y3-Z2-FILLET", "X1-Y3-Z2", "X1-Y4-Z1", "X1-Y4-Z2", "X2-Y2-Z2-FILLET", "X2-Y2-Z2"};
+        vector<Vector3d> vectors=populateVector();
+        for(int i=0; i<names.size(); i++) {
+            this->validBlocks.pushBack("model://"+names[i]);
+            this->brickDefinition(pair<string, gazebo::BlockSize>("model://"+names[i], vectors[i]));
+        }
+        this->worldPtr=_parent;
+    }
+    void generateBlocks() {
+        
+    }
+    void spawnCallback(const shared_ptr<msgs::Factory>& msg) {
+       factory_ = msg;
+       RCLCPP_INFO(this->get_logger(), "Received block spawn message: %s", factory_->name.c_str());
+       generateBlocks();
+   }
+}
+string random_name();
+
+#endif
--- robotics_project_ws/block_spawner_ws/src/block_spawn.cpp
+++ robotics_project_ws/block_spawner_ws/src/block_spawn.cpp
@@ -1,79 +1,35 @@
-#include <rclcpp.hpp>
-#include <string>
-#include <Eigen/Dense>
-#include "gazebo/physics/physics.hh"
-#include "gazebo/common/common.hh"
-#include "gazebo/gazebo.hh"
+#include "block_spawner.h"
+BlockSpawn::BlockSpawn(const string& path): Node("block_spawning") {
+    RCLCPP_INFO(this->get_logger(), "BlockSpawner Node Initialized");
+    gazebo_msgs::SpawnModel spawner;
+    spawner.request.model_name=random_name();
+    spawner.request.model_xml="/home/ubuntu/ros2_ws/src/ros2_ur5_interface/package.xml";
+    block_spawner_ = this->create_subscription<msgs::Factory>(
+    "/block_spawner_node/spawn", 100,
+        std::bind(&BlockSpawn::spawnCallback, this, std::placeholders::_1)
+    );
+    rclcpp::spin_some(this->get_node_base_interface());
+}
+string random_name() {
+    stringstream ss;
+    ss << hex << rand();
+    return ss.str();
+}
+int main (int argc, const char* argv[]) {
+    rclcpp::init(argc, argv);
+    cout << "Start Spawning Blocks" << endl;
+    RCLCPP_INFO(this->get_logger(), "Spawn Blocks");
+    auto node=make_shared<BlockSpawn>("/home/ubuntu/ros2_ws/src/ros2_ur5_interface/models/meshed");
+    try {
+        while(rclcpp::ok()) {
+            rclcpp::spinOnce();
+        }
+        cout << "End Spawning Blocks" << endl;
+    }
+    catch (const exception& e) {
+        RCLCPP_ERROR(this->get_logger(), "Error: %s", e.what());
+    }
+    rclcpp::shutdown();
+    return 0;
+}
 
-#define NUM_BLOCKS 5
-#define MINIMUM_DISTANCE_BLOCK 0.15
-#define BORDER_DISTANCE 0.05
-#define DISTANCE_BOTTOM 0.4
-using namespace std;
-using namespace gazebo {
-struct BlockSize {
-    double x;
-    double y;
-    double z;
-};
-struct BlockPosition {
-    string name;
-    Vector3d wPosition;
-}
-class BlockSpawn : public rclcpp::Node : public WorldPlugin {
-public:
-    BlockSpawn(): Node("block_spawning") {
-        RCLCPP_INFO(this->get_logger(), "BlockSpawner Node Initialized");
-        block_spawner_ = this->create_subscription<msgs::Factory>(
-        "/block_spawner_node/spawn", 100,
-            std::bind(&BlockSpawn::spawnCallback, this, std::placeholders::_1)
-        );
-        rclcpp::spin_some(this->get_node_base_interface());
-    }
-    void Load(physics::WorldPtr _parent, sdf::ElementPtr) {
-        initVariables(physics::WorldPtr _parent);
-        generateBlocks();
-        block_spawner_=this->create_subscription<msgs::Factory>("/block_spawner_node/spawn", 100, [this](shared_ptr<msgs::Factory> msg){
-            factory_= const_pointer_cast<const msgs::Factory>(msg);
-            RCLCPP_INFO(this->get_logger(), "Set Block Spawning Message");
-        });
-        while (rclcpp::ok() && !block_spawner_) {
-            rclcpp::spin_some(this->get_node_base_interface());
-        }
-        if(block_spawner_) {
-            RCLCPP_INFO(this->get_logger(), "Successfully Spawned Blocks");
-            cout << "Successfully Spawned Blocks" << endl;
-        }
-    }
-    
-private:
-    vector<string> validBlocks;
-    map<string, gazebo::BlockSize> brickDefinition;
-    physics::WorldPtr worldPtr;
-    shared_ptr<rclcpp::Subscription<msgs::Factory>> block_spawner_;
-    shared_ptr<const msgs::Factory> factory_;
-    vector<gazebo::BlockPosition> addedBuffer;
-    vector<Vector3d> populateVector() {
-        //To implement reading in C++
-    }
-    void initVariables(physics::WorldPtr _parent) {
-        this->validBlocks.clear();
-        vector<string> names={"X1-Y1-Z2", "X1-Y2-Z1", "X1-Y2-Z2-CHAMFER", "X1-Y2-Z2-TWINFILLET", "X1-Y2-Z2", "X1-Y3-Z2-FILLET", "X1-Y3-Z2", "X1-Y4-Z1", "X1-Y4-Z2", "X2-Y2-Z2-FILLET", "X2-Y2-Z2"};
-        vector<Vector3d> vectors=populateVector();
-        for(int i=0; i<names.size(); i++) {
-            this->validBlocks.pushBack("model://"+names[i]);
-            this->brickDefinition(pair<string, gazebo::BlockSize>("model://"+names[i], vectors[i]));
-        }
-        this->worldPtr=_parent;
-    }
-    void generateBlocks() {
-        
-    }
-    void spawnCallback(const shared_ptr<msgs::Factory>& msg) {
-       factory_ = msg;
-       RCLCPP_INFO(this->get_logger(), "Received block spawn message: %s", factory_->name.c_str());
-       generateBlocks();
-   }
-}
-GZ_REGISTER_WORLD_PLUGIN(BlockSpawn);
-}
