09b4ca55666cf07c3b1825737f32aa6497c6a12b
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-12-19 13:52:41 +0100

    Update path.h

--- robotics_project_ws/src/move_ws/include/path.h
+++ robotics_project_ws/src/move_ws/include/path.h
@@ -2,21 +2,40 @@
 //  path.h
 //  Robotics
 //
-//  Created by Niccolò Cristoforetti on 05/12/24.
+//  Created by Niccol� Cristoforetti on 05/12/24.
 //
 
-#ifndef path_h
-#define path_h
+#ifndef __PATH_H__
+#define __PATH_H__
 #include <iostream>
-#include "kinematics.h"
-#include "kin_communication.h"
-#include <iostream>
+#include "kinematics.h" 
+#include "kin_communication.h" 
 #include <vector>
-#include <utility>
-#include <cmath>
-#include <Eigen/Dense>
+#include <utility> //check
+#include <cmath> //check
+#include <Eigen/Dense> 
+#include <string>  
+//#include <format>
+#include <rclcpp/rclcpp.hpp> 
+#include "move_ws/msg/vector3d.hpp"
+#include "move_ws/srv/moving.hpp"
 
-bool path(Vector3d xE1, Vector3d phiE1, int argc, const char* argv[]);
+using namespace std;
+
+bool path(Vector3d xE1, Vector3d phiE1, int argc, const char* argv[]);  
+
+using MoveService=move_ws::srv::Moving;
+
+class MyVector : public rclcpp::Node {
+public:
+    MyVector();
+    void calculatePath(
+        const shared_ptr<MoveService::Request> request,
+                   shared_ptr<MoveService::Response> response);
+private:
+    shared_ptr<rclcpp::Service<MoveService>> service_;
+};
 
 #endif /* path_h */
+
 
