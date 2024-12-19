b6c52814f34457b9f762a6425329742f5d0a7c4a
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-12-19 15:02:31 +0100

    Update testMove.sh

--- testMove.sh
+++ testMove.sh
@@ -3,8 +3,8 @@ cd /home/ubuntu/ros2_ws/
 
 rosdep install -i --from-path src --rosdistro rolling -y
 
-colcon build --packages-select move_ws_pkg
+colcon build --packages-select move_ws
 
 source install/setup.bash
 
-ros2 run move_ws_pkg p2pmotion
+ros2 run move_ws service_moving
