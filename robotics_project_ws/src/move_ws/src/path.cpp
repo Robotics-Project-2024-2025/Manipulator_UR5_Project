451e047cdfe1727a4a8ceb6ff6f682d41b7e326c
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-12-19 13:50:09 +0100

    Update path.cpp

--- robotics_project_ws/src/move_ws/src/path.cpp
+++ robotics_project_ws/src/move_ws/src/path.cpp
@@ -2,12 +2,13 @@
 //  path.cpp
 //  Robotics
 //
-//  Created by Niccolò Cristoforetti on 05/12/24.
+//  Created by Niccol� Cristoforetti on 05/12/24.
 //
 
 #include "path.h"
+using namespace std;
 
-bool path(Vector3d xE1, Vector3d phiE1, int argc, const char* argv[]){
+bool path(Vector3d xe1, Vector3d phie1, int argc, const char* argv[]){
     Matrix16 qES;
     qES=receive_joint_state();
     cout << qES << endl;
@@ -15,10 +16,10 @@ bool path(Vector3d xE1, Vector3d phiE1, int argc, const char* argv[]){
     int j=0;
     do {
         double tmp;
-        cout << "Insert value " << j+1 << "for xE1: ";
+        cout << "Insert value " << j+1 << "for xe1: ";
         cin >> tmp;
         if(tmp >= -1 && tmp <= 1){
-            xE1(j) = tmp;
+            xe1(j) = tmp;
             j++;
         }
         else{
@@ -30,25 +31,83 @@ bool path(Vector3d xE1, Vector3d phiE1, int argc, const char* argv[]){
     cout << "Insert Time: ";
     cin >> time;
     cout << "Checking Position" << endl;
-    if(checkPosition(xE1, qES)) {
-        if(p2pMotionPlan(qES, xE1, phiE1, time, &th)) {
+    if(checkPosition(xe1, qES)) {
+        if(p2pMotionPlan(qES, xe1, phie1, time, &th)) {
             cout << "Moving to HOME" << endl;
             setupCommunication(argc, argv);
             send_trajectory(th);
         }
         else {
-            cout << "Error in computation of trajectory to point " << xE1 << endl;
+            cout << "Error in computation of trajectory to point " << xe1 << endl;
             return false;
         }
     }
     else {
-        cout << "Impossible to Reach the final Position " << xE1 << endl;
+        cout << "Impossible to Reach the final Position " << xe1 << endl;
         return false;
     }
-    cout << "Control vector xE1 integrity:";
+    cout << "Control vector xe1 integrity:";
     for(int i=0; i<3; i++){
-        cout << endl << xE1(i) << " ";
+        cout << endl << xe1(i) << " ";
     }
     setupCommunication(argc, argv);
     return true;
+}
+
+//ImageCamera::ImageCamera(Mode mode) : Node("image_acquiring") {
+MyVector::MyVector() : Node("path_acquiring") {
+        //service_=this->create_service<serviceDepth>("service_depth", bind(&ImageCamera::calculateDepth, this, placeholders::_1, placeholders::_2));
+        service_=this->create_service<MoveService>("service_path", bind(&MyVector::calculatePath, this, std::placeholders::_1, std::placeholders::_2));
+        auto shared_this=shared_ptr<MyVector>(this);
+        while(rclcpp::ok()) {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to take path vectors xe1(x,y,z) phie1(x,y,z)");
+            rclcpp::spin(shared_this);
+        }
+}
+
+
+void MyVector::calculatePath(
+    const shared_ptr<MoveService::Request> request,
+    shared_ptr<MoveService::Response> response){
+    Matrix16 qES;
+    qES=receive_joint_state();
+    //cout << qES << endl;
+    MatrixD6 th;
+    double time = 4.0;
+    Vector3d v, p;
+    v << request->xe1.x, request->xe1.y, request->xe1.z;
+    p << request->phie1.x, request->phie1.y, request->phie1.z;
+    //cout << "Checking Position" << endl;
+    if(checkPosition(v, qES)) {
+        if(p2pMotionPlan(qES, v, p, time, &th)) {
+            //cout << "Moving to HOME" << endl;
+            //setupCommunication(request->argc, request->argv);
+            send_trajectory(th);
+        }
+        else {
+            //cout << "Error in computation of trajectory to point " << request->xe1 << endl;
+            //return false;
+            response->result = false;
+        }
+    }
+    else {
+        //cout << "Impossible to Reach the final Position " << request->xe1 << endl;
+        //return false;
+        response->result = false;
+    }
+    //cout << "Control vector xe1 integrity:";
+   /*for(int i=0; i<3; i++){
+        cout << endl << request->xe1(i) << " ";
+    }
+    */
+    //setupCommunication(request->argc, request->argv);
+    //return true;
+    response->result = true;
+}
+
+int main (int argc, const char* argv[]) {
+    rclcpp::init(argc, argv);
+    auto node_s=make_shared<MyVector>();
+    rclcpp::shutdown();
+    return 0;
 }
