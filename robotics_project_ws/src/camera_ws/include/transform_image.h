f00b1100f2d01c843c38a98c0c357e2ea5a88366
niccolocristoforetti <113518101+niccolocristoforetti@users.noreply.github.com>
2024-12-20 12:30:15 +0100

    Update transform_image.h

--- robotics_project_ws/src/camera_ws/include/transform_image.h
+++ robotics_project_ws/src/camera_ws/include/transform_image.h
@@ -11,7 +11,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <opencv2/opencv.hpp>
-#include "camera_ws/srv/three_dto2_d.hpp"
+#include "camera_ws/srv/conversion.hpp"
 
 class TableTransformService : public rclcpp::Node {
 
