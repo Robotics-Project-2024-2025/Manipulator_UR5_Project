3c5a0d608c8bcbf612ff5cd7f98ce761373e204c
ale-bena <127418373+ale-bena@users.noreply.github.com>
2024-12-20 11:04:47 +0100

    Update detection.py

--- robotics_project_ws/src/vision_ws/launch/detection.py
+++ robotics_project_ws/src/vision_ws/launch/detection.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from example_interfaces.srv import Trigger  # Or define your custom service
+from boundingbox.srv import Trigger
 from ultralytics import YOLO  
 
 class YoloBoundingBoxService(Node):
