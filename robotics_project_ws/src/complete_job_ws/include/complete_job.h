23130aca7b53ff519df060d8f2bcf9c5632cc88c
ale-bena <127418373+ale-bena@users.noreply.github.com>
2025-01-24 10:47:05 +0100

    Update complete_job.h

--- robotics_project_ws/src/complete_job_ws/include/complete_job.h
+++ robotics_project_ws/src/complete_job_ws/include/complete_job.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include "camera_ws/srv/conversion.hpp"
+#include "vision_ws_msgs/srv/boundingbox.hpp"
 #include "std_srvs/srv/trigger.hpp"
 #include <Eigen/Dense>
 #include "kin_communication.h"
